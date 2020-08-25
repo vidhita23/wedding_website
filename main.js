004eadc81a76a2fc1288b5d841ec5c90b38e4f30
vidhita <sjnx2311@gmail.com>
2020-08-26 00:49:35 +0530

    commit-7

--- main.js
+++ main.js
@@ -4,60 +4,53 @@ const next = document.querySelector(".next");
 const prev = document.querySelector(".prev");
 const slides = document.querySelectorAll(".slide");
 
-
 let index = 0;
 display(index);
 
-function display(index)
-{
-    slides.forEach((slide) => {
-        slide.style.display="none";
-    });
-    slides[index].style.display="flex"
+function display(index) {
+  slides.forEach((slide) => {
+    slide.style.display = "none";
+  });
+  slides[index].style.display = "flex";
 }
 
-
-function nextSlide(){
-    index++;
-    if(index > slides.length-1)
-    {
-        index=0;
-    }
-   display(index);
+function nextSlide() {
+  index++;
+  if (index > slides.length - 1) {
+    index = 0;
+  }
+  display(index);
 }
 
-function prevSlide(){
-    index--;
-    if(index < 0)
-    {
-        index = slides.length-1;
-    }
-    display(index);
+function prevSlide() {
+  index--;
+  if (index < 0) {
+    index = slides.length - 1;
+  }
+  display(index);
 }
 
-next.addEventListener("click" , nextSlide);
-prev.addEventListener("click" , prevSlide);
-
-
+next.addEventListener("click", nextSlide);
+prev.addEventListener("click", prevSlide);
 
 // -------countdown
 
 let count = new Date("Jan 1,2021,00:00:00").getTime();
 
-let x = setInterval(function(){
-    let now = new Date().getTime();
-        var d = count-now;
+let x = setInterval(function () {
+  let now = new Date().getTime();
+  var d = count - now;
 
-    let days = Math.floor(d/(1000*60*60*24));
-    let hours = Math.floor((d % (1000*60*60*24)) / (1000*60*60));
-    let minutes =Math.floor((d % (1000*60*60)) / (1000*60));
-    let seconds =Math.floor((d % (1000*60)) / (1000));
+  let days = Math.floor(d / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((d % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((d % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((d % (1000 * 60)) / 1000);
 
-    document.getElementById("day").innerHTML = days;
-    document.getElementById("hour").innerText = hours;
-    document.getElementById("minute").innerText = minutes;
-    document.getElementById("second").innerText = seconds;
-},1000);
+  document.getElementById("day").innerHTML = days;
+  document.getElementById("hour").innerText = hours;
+  document.getElementById("minute").innerText = minutes;
+  document.getElementById("second").innerText = seconds;
+}, 1000);
 
 //--------------------------- memory
 
@@ -76,12 +69,12 @@ showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
@@ -89,27 +82,31 @@ function showSlides(n) {
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demons");
   var captionText = document.getElementById("mem-caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
 }
-
 
 // header
-const headSlides = () => {
-  const burger = document.querySelector(".burger");
-  const head = document.querySelector(".head-links");
+// const header = document.querySelector("nav");
+// const sectionOne = document.querySelector("slider");
 
-  burger.addEventListener("click",() => {
-    navigator.classList.toggle(".head-active");
-  });
-}
-headSlides();
+// const selectionOneOptions = {
+//   rootMargin: "-200px 0px 0px 0px"
+// };
+
+// const selectionOneObserver = new IntersectionObserver
+// (function(entries , selectionOneObserver )
+// )
